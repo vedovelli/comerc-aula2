890af915c4d2486dbe80a93015d9fec4b53cfada
Fabio Vedovelli <vedovelli@gmail.com>
2018-06-19 14:18:53 -0300

    http e event bu plugins criados e instalados

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import http from '@p/http'
+import eventBus from '@p/event-bus'
 
 Vue.config.productionTip = false
+
+Vue.use(http)
+Vue.use(eventBus)
 
 /* eslint-disable no-new */
 new Vue({
