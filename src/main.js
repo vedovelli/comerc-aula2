c52fc37920275a95d57239dcf4ca638635170ab0
Fabio Vedovelli <vedovelli@gmail.com>
2018-06-19 14:42:17 -0300

    Vuex Store configurada e instalada

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import plugins from '@p'
+import store from '@/store'
 
 Vue.config.productionTip = false
 
@@ -12,5 +13,6 @@ Vue.use(plugins)
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
