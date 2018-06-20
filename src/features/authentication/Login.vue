da1e81810789500081da759f98b93949d8638ef5
Fabio Vedovelli <vedovelli@gmail.com>
2018-06-20 15:34:25 -0300

    Layout para Login

--- src/features/authentication/Login.vue
+++ src/features/authentication/Login.vue
@@ -5,6 +5,34 @@ export default {}
 
 <template>
   <div>
-    <h1>Login</h1>
+    <div class="panel panel-default panel-login">
+      <div class="panel-heading">
+        <h4>Bem vindo a Comerc</h4>
+      </div>
+      <form action="#" class="panel-body">
+        <div class="form-group">
+          <div class="control-label">E-mail</div>
+          <input type="email" class="form-control">
+        </div>
+        <div class="form-group">
+          <div class="control-label">Password</div>
+          <input type="password" class="form-control">
+        </div>
+        <button class="btn btn-block btn-primary">Acessar</button>
+        <p class="password-link">
+          <router-link :to="{ name: 'password.index' }">Esqueci minha senha</router-link>
+        </p>
+      </form>
+    </div>
   </div>
 </template>
+
+<style>
+  .panel-login {
+    width: 400px;
+    margin: 0 auto;
+  }
+  .password-link {
+    margin-top: 15px;
+  }
+</style>
