e4c95bc06bbaf9e95a7e9993ed8b19732aa7cdf6
Fabio Vedovelli <vedovelli@gmail.com>
2018-06-21 15:19:15 -0300

    Table para a lista de produtos

--- src/features/products/Main.vue
+++ src/features/products/Main.vue
@@ -6,5 +6,24 @@ export default {}
 <template>
   <div>
     <h1>Produtos</h1>
+    <table class="table table-bordered table-stripped table-hover">
+      <thead>
+        <tr>
+          <th class="text-center">ID</th>
+          <th>Produto</th>
+          <th class="text-center">Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td width="75" class="text-center">100</td>
+          <td>Nome do Produto</td>
+          <td width="150" class="text-center">
+            [ <a href="#">editar</a> ]
+            [ <a href="#">excluir</a> ]
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
